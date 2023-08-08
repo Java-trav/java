d1fda1369b44c93e944a47efbdd64d3c6119893c
Travis Gomez <frank.travis.gomez@gmail.com>
2023-08-07 22:04:13 -0500

    increment of i using a while loop

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class ControlFlowExercises {
+    public static void main(String[] args){
+
+        int i = 5;
+        //while loop that is less than or equal to 15
+        while(i <= 15){
+            // this is the current output or i
+            System.out.print(i + " ");
+            // this is the incrememnt of i
+            i++;
+        }
+    }
+}
