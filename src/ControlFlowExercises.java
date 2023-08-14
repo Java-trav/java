99d648c174da5ae8eba67b337e7c435f0fe10b88
Travis Gomez <frank.travis.gomez@gmail.com>
2023-08-14 15:45:22 -0500

    re factored with grades plus and minus

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -81,23 +81,57 @@ public class ControlFlowExercises {
         Scanner scanner = new Scanner(System.in);
         String answer;
         // do while loop with conditionals inside that prints different grade options
+//        do{
+//            System.out.println("Please enter a numerical grade: ");
+//            int grade = scanner.nextInt();
+//            if(grade >= 88 && grade <=100){
+//                System.out.println("your grade is an A!");
+//            }else if(grade >=80 && grade <= 87){
+//                System.out.println("your grade is a B.");
+//            }else if(grade >=67 && grade <= 79){
+//                System.out.println("your grade is a C... could B better...");
+//            }else if(grade >=60 && grade <= 66){
+//                System.out.println("your grade is a D... did you study?");
+//            }else{
+//                System.out.println("your grade is a F... FATALITY!");
+//            }
+//            System.out.print("Do you want to continue? (yes or no): ");
+//            answer = scanner.next();
+//        }while(answer.equalsIgnoreCase("yes"));
+
         do{
-            System.out.println("Please enter a numerical grade: ");
+            System.out.println("Please enter a numerical number: ");
             int grade = scanner.nextInt();
-            if(grade >= 88 && grade <=100){
-                System.out.println("your grade is an A!");
-            }else if(grade >=80 && grade <= 87){
-                System.out.println("your grade is a B.");
-            }else if(grade >=67 && grade <= 79){
-                System.out.println("your grade is a C... could B better...");
-            }else if(grade >=60 && grade <= 66){
-                System.out.println("your grade is a D... did you study?");
+            if(grade >= 97 && grade <= 100){
+                System.out.println("Congrats A+!");
+            }else if(grade >= 93 && grade <= 96){
+                System.out.println("Congrats on getting an A!");
+            }else if(grade >= 90 && grade <= 92){
+                System.out.println("Not bad but not the best A-");
+            }else if(grade >= 87 && grade <= 89){
+                System.out.println("Almost an A but it's a B+!");
+            }else if(grade >= 83 && grade <= 86){
+                System.out.println("It's just a B");
+            }else if(grade >= 80 && grade <= 82){
+                System.out.println("Not good B-");
+            }else if(grade >= 77 && grade<= 79){
+                System.out.println("Did you study mr C+");
+            }else if(grade >= 73 && grade <= 76){
+                System.out.println("just a C");
+            }else if(grade >= 70 && grade <= 72){
+                System.out.println("you should study more C-");
+            }else if(grade >=67 && grade <= 69){
+                System.out.println("D+");
+            }else if(grade >= 65 && grade <= 66){
+                System.out.println("D");
+            }else if(grade >= 64){
+                System.out.println("Seriously study D-");
             }else{
                 System.out.println("your grade is a F... FATALITY!");
             }
+
             System.out.print("Do you want to continue? (yes or no): ");
             answer = scanner.next();
         }while(answer.equalsIgnoreCase("yes"));
-
     }
 }
