cd535ef2df737968037d6d05cc7e90a601d8e8ce
Travis Gomez <frank.travis.gomez@gmail.com>
2023-08-15 21:10:20 -0500

    java string basics

---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String msg = "We don't need no education\n";
+         msg = msg + "We don't need no thought control";
+        System.out.println(msg);
+    }
+}
